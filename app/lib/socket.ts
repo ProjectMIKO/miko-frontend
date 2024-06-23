289f85a478936858e4192de83c6991494cada597
hkyoo <hkyoo988@naver.com>
2024-06-23 17:41:20 +0900

    refactor: socket url

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "https://miko-server-894bf0dedc4e.herokuapp.com"; // 소켓 서버 URL
+const SOCKET_URL = "https://miko-dev-a7d3f7eaf040.herokuapp.com"; // 소켓 서버 URL
 
 const socket: Socket = io(SOCKET_URL, {
   autoConnect: false, // 자동 연결 비활성화
