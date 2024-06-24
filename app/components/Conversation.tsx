433712ff354b0933cfeedf853de29d7355ada860
GEONU-MOON <moondy2209@naver.com>
2024-06-24 12:59:36 +0900

    #35 feat: 대화, 키워드 탭 전환 구현

--- app/components/Conversation.tsx
+++ app/components/Conversation.tsx
@@ -1,65 +1,45 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useSocket } from '../components/SocketContext';
+import React from "react";
 
+interface ConversationProps {
+  messages: string[];
+}
 
-const NodeList: React.FC = () => {
-    const { socket } = useSocket();
-    const [messages, setMessages] = useState<string[]>([]);
-    const handleNewMessage = useRef((message: string) => {
-        setMessages(prevMessages => [...prevMessages, message]);
-      });
-
-    useEffect(() => {
-        const handleMessage = (message: string) => {
-            console.log('Received message from server:', message);
-            handleNewMessage.current(message);
-          };
-    
-        socket.on('script', (message: string) => {
-            console.log('Received message from server:', message);
-            handleMessage(message);
-        });
-
-        return () => {
-            socket.off('script', handleMessage);
-        };
-    }, [socket]);
-
-    return (
-        <div
+const Conversation: React.FC<ConversationProps> = ({ messages }) => {
+  return (
+    <div
+      style={{
+        width: "300px",
+        margin: "5px",
+        border: "1px solid #CCC",
+        padding: "10px",
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+        borderRadius: "8px",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#F9F9F9",
+        maxHeight: "650px",
+        overflowY: "auto",
+      }}
+    >
+      <h3 style={{ textAlign: "center", color: "#333" }}>Conversation</h3>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {messages.map((message, index) => (
+          <li
+            key={index}
             style={{
-                width: "300px",
-                margin: "5px",
-                border: "1px solid #CCC",
-                padding: "10px",
-                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-                borderRadius: "8px",
-                fontFamily: "Arial, sans-serif",
-                backgroundColor: "#F9F9F9",
-                maxHeight: "650px", // 최대 높이 설정
-                overflowY: "auto", // 수직 스크롤 추가
+              cursor: "pointer",
+              padding: "10px",
+              border: "1px solid #CCC",
+              borderRadius: "4px",
+              marginBottom: "10px",
+              transition: "background-color 0.3s, color 0.3s, box-shadow 0.3s",
             }}
-        >
-            <h3 style={{ textAlign: "center", color: "#333" }}>Conversation</h3>
-            <ul style={{ listStyleType: "none", padding: 0 }}>
-                {messages.map((message, index) => (
-                    <li
-                        key={index}
-                        style={{
-                            cursor: "pointer",
-                            padding: "10px",
-                            border: "1px solid #CCC",
-                            borderRadius: "4px",
-                            marginBottom: "10px",
-                            transition: "background-color 0.3s, color 0.3s, box-shadow 0.3s",
-                        }}
-                    >
-                        {message}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+          >
+            {message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default NodeList;
+export default Conversation;
---
+++ app/components/NodeCoversation.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useRef } from "react";
+import NodeList from "./NodeList";
+import Conversation from "./Conversation";
+import styles from "../Home.module.css";
+import { Node } from "../../types/types";
+import { useSocket } from "../components/SocketContext";
+
+interface NodeConversationProps {
+  nodes: Node[];
+  selectedNodeId: number | null;
+  onNodeClick: (nodeId: number) => void;
+}
+
+const NodeConversation: React.FC<NodeConversationProps> = ({
+  nodes,
+  selectedNodeId,
+  onNodeClick,
+}) => {
+  const [activeTab, setActiveTab] = useState<string>("nodes");
+  const [messages, setMessages] = useState<string[]>([]);
+  const { socket } = useSocket();
+  const handleNewMessage = useRef((message: string) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  });
+
+  useEffect(() => {
+    const handleMessage = (message: string) => {
+      console.log("Received message from server:", message);
+      handleNewMessage.current(message);
+    };
+
+    socket.on("script", handleMessage);
+
+    return () => {
+      socket.off("script", handleMessage);
+    };
+  }, [socket]);
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "nodes":
+        return (
+          <NodeList
+            nodes={nodes}
+            selectedNodeId={selectedNodeId}
+            onNodeClick={onNodeClick}
+          />
+        );
+      case "conversation":
+        return <Conversation messages={messages} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "300px",
+        margin: "5px",
+        border: "1px solid #CCC",
+        padding: "10px",
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+        borderRadius: "8px",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#F9F9F9",
+        maxHeight: "650px",
+        overflowY: "auto",
+      }}
+    >
+      <div className={styles.tabContainer}>
+        <button
+          className={activeTab === "nodes" ? styles.activeTab : styles.tab}
+          onClick={() => setActiveTab("nodes")}
+        >
+          Node List
+        </button>
+        <button
+          className={
+            activeTab === "conversation" ? styles.activeTab : styles.tab
+          }
+          onClick={() => setActiveTab("conversation")}
+        >
+          Conversation
+        </button>
+      </div>
+      {renderContent()}
+    </div>
+  );
+};
+
+export default NodeConversation;
