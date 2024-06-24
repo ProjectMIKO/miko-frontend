cd760b3e3a4fcc4bd825068d1be2b0e63c11f4ab
hkyoo <hkyoo988@naver.com>
2024-06-24 21:16:17 +0900

    test: stt

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -10,28 +10,54 @@ const Home: React.FC = () => {
     const [messages, setMessages] = useState<string[]>([]);
     const handleNewMessage = useRef((message: string) => {
         setMessages((prevMessages) => [...prevMessages, message]);
-      });
+    });
 
     useEffect(() => {
-        // 예제 닉네임으로 소켓 연결을 시도합니다.
         const nickname = 'testUser';
+        const sessionId = 'testRoomName';
+
+        // 소켓 연결
         connectSocket(nickname);
+
+        // 연결 후 방 입장
+        socket.on('connect', () => {
+            socket.emit('enter_room', sessionId);
+
+            socket.on('entered_room', () => {
+                console.log('Entered room:', sessionId);
+
+              });
+        });
         const handleMessage = (message: string) => {
             console.log("Received message from server:", message);
             handleNewMessage.current(message);
-          };
+        };
 
         socket.on("script", handleMessage);
+
+
+
+        // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
         return () => {
+            socket.off("connect");
+            socket.off("entered_room");
             socket.off("script");
+            socket.off("enter_room");
+            
             disconnectSocket();
         };
+
     }, [connectSocket, disconnectSocket]);
 
     return (
         <div>
             <h1>Welcome to Voice Test</h1>
-            <VoiceComponent/>
+            <VoiceComponent />
+            <div>
+                {messages.map((message, index) => (
+                    <p key={index}>{message}</p>
+                ))}
+            </div>
         </div>
     );
 };
