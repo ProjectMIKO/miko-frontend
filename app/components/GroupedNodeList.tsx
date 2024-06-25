551c90a9d1aebacd24628f54b0adf9c2c211af7b
GEONU-MOON <moondy2209@naver.com>
2024-06-25 15:57:15 +0900

    refactor : types폴더 하나로 통일

--- app/components/GroupedNodeList.tsx
+++ app/components/GroupedNodeList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Node, Edge } from "../../types/types";
+import { Node, Edge } from "../types/types";
 
 interface GroupedNodeListProps {
   nodes: Node[];
