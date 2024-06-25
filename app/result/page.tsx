609ff43e5a41185e50c8bdd4f9df6a5f922745b7
GEONU-MOON <moondy2209@naver.com>
2024-06-25 16:45:46 +0900

    #47 feat : result페이지 기본 레이아웃 완성

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Page.module.css";
 
 const Page = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "tab1":
+        return <div>탭 1의 내용</div>;
+      case "tab2":
+        return <div>탭 2의 내용</div>;
+      case "tab3":
+        return <div>탭 3의 내용</div>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,8 +31,36 @@ const Page = () => {
       <header className={styles.header}>MIKO</header>
 
       <main className={styles.main}>
-        <section className={styles.left}>왼쪽 영역</section>
-        <section className={styles.right}>오른쪽 영역</section>
+        <section className={styles.left}>노드 그래프 영역</section>
+        <section className={styles.right}>
+          <div className={styles.tabs}>
+            <button
+              onClick={() => setActiveTab("tab1")}
+              className={`${styles.tabButton} ${
+                activeTab === "tab1" ? styles.activeTab : ""
+              }`}
+            >
+              그룹
+            </button>
+            <button
+              onClick={() => setActiveTab("tab2")}
+              className={`${styles.tabButton} ${
+                activeTab === "tab2" ? styles.activeTab : ""
+              }`}
+            >
+              키워드 요약
+            </button>
+            <button
+              onClick={() => setActiveTab("tab3")}
+              className={`${styles.tabButton} ${
+                activeTab === "tab3" ? styles.activeTab : ""
+              }`}
+            >
+              음성 기록
+            </button>
+          </div>
+          <div className={styles.tabContent}>{renderTabContent()}</div>
+        </section>
       </main>
 
       <footer className={styles.footer}>
