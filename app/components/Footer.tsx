005fd7828561bd7aa78641bf39de4c7101d2981a
GEONU-MOON <moondy2209@naver.com>
2024-06-25 21:41:43 +0900

    #52 refactor : 결과화면 컴포넌트 리팩토링

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import styles from "../styles/Footer.module.css";
+
+interface FooterProps {
+  children: ReactNode;
+}
+
+const Footer: React.FC<FooterProps> = ({ children }) => (
+  <footer className={styles.footer}>{children}</footer>
+);
+
+export default Footer;
---
+++ app/components/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+
+const Header = () => <header className={styles.header}>MIKO</header>;
+
+export default Header;
