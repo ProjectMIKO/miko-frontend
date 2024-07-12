1a1f6d0ba48e24a413449df8c9f30807f7f11c1f
Cheongwoo Nam <skacjddn7@gmail.com>
2024-07-12 15:03:41 +0900

    refactor: conversation 말풍선 css 적용 및 유저 이미지 추가 #157

--- app/waiting/page.tsx
+++ app/waiting/page.tsx
@@ -30,6 +30,8 @@ const WaitingPage: React.FC = () => {
     if (session?.user?.name) {
       setMyUserName(session.user.name);
       localStorage.setItem("userName", session.user.name); // 유저 이름을 로컬스토리지에 저장
+      if (session?.user?.image)
+        localStorage.setItem("userImage", session.user.image); // 유저 사진을 로컬스토리지에 저장
     } else {
       const randomUserName = "OpenVidu_User_" + Math.floor(Math.random() * 100);
       setMyUserName(randomUserName);
